97b010a622bb28e935922847329a27b95d8e8c9f
dor1987 <dorash878@gmail.com>
2018-01-22 12:11:33 +0200

    Starting point

---
+++ android/src/com/dorashush/defenders/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dorashush.defenders;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dorashush.defenders.Defenders;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Defenders(), config);
+	}
+}
---
+++ desktop/src/com/dorashush/defenders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dorashush.defenders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dorashush.defenders.Defenders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Defenders(), config);
+	}
+}
